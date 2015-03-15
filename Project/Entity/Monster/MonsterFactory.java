96c826590cb233b9313d6ebec47a9d5504e1128a
Amber Wise <awise@eagles.ewu.edu>
2015-03-14 23:20:16 -0700

    Blue and green sprite added

--- Project/Entity/Monster/MonsterFactory.java
+++ Project/Entity/Monster/MonsterFactory.java
@@ -19,72 +19,72 @@ public class MonsterFactory
       
       if(type.equals("Basilisk"))
       {
-         //monster = new Basilisk();
+         monster = new Basilisk();
          monster.setMonsterName(name);
       }
       else if (type.equals("Chimera"))
       {
-         //monster = new Chimera();
+         monster = new Chimera();
          monster.setMonsterName(name);
       }
       else if (type.equals("Chimera Skeleton"))
       {
-         //monster = new ChimeraSkeleton();
+         monster = new ChimeraSkeleton();
          monster.setMonsterName(name);
       }
       else if (type.equals("Gargoyle"))
       {
-         //monster = new Gargoyle();
+         monster = new Gargoyle();
          monster.setMonsterName(name);
       }
       else if (type.equals("Gorgon"))
       {
-         //monster = new Gorgon();
+         monster = new Gorgon();
          monster.setMonsterName(name);
       }
       else if (type.equals("Gryphon"))
       {
-         //monster = new Gryphon();
+         monster = new Gryphon();
          monster.setMonsterName(name);
       }
       else if (type.equals("Hydra"))
       {
-         //monster = new Hydra();
+         monster = new Hydra();
          monster.setMonsterName(name);
       }
       else if (type.equals("Lich"))
       {
-         //monster = new Lich();
+         monster = new Lich();
          monster.setMonsterName(name);
       }
       else if (type.equals("Mummy"))
       {
-         //monster = new Mummy();
+         monster = new Mummy();
          monster.setMonsterName(name);
       }
       else if (type.equals("Skeleton"))
       {
-         //monster = new Skeleton();
+         monster = new Skeleton();
          monster.setMonsterName(name);
       }
       else if (type.equals("Vampire"))
       {
-         //monster = new Vampire();
+         monster = new Vampire();
          monster.setMonsterName(name);
       }
       else if (type.equals("Worg"))
       {
-         //monster = new Worg();
+         monster = new Worg();
          monster.setMonsterName(name);
       }
       else if (type.equals("Wraith"))
       {
-         //monster = new Wraith();
+         monster = new Wraith();
          monster.setMonsterName(name);
       }
       else if (type.equals("Zombie"))
       {
-         //monster = new Zombie();
+         monster = new Zombie();
          monster.setMonsterName(name);
       }
       return monster;
